227d5f76916eb92dbf73cc866e12c00d94a298d7
Restyled.io <commits@restyled.io>
2024-01-20 09:27:01 +0000

    Restyled by black

--- kmeans_elbow.py
+++ kmeans_elbow.py
@@ -4,22 +4,28 @@ from sklearn.cluster import KMeans
 
 def elbow_plot(data, maxK=10, seed_centroids=None):
     """
-        parameters:
-        - data: pandas DataFrame (data to be fitted)
-        - maxK (default = 10): integer (maximum number of clusters with which to run k-means)
-        - seed_centroids (default = None ): float (initial value of centroids for k-means)
+    parameters:
+    - data: pandas DataFrame (data to be fitted)
+    - maxK (default = 10): integer (maximum number of clusters with which to run k-means)
+    - seed_centroids (default = None ): float (initial value of centroids for k-means)
     """
     sse = {}
     for k in range(1, maxK):
         # Removed unnecessary print statement
         if seed_centroids is not None:
             seeds = seed_centroids.head(k)
-            kmeans = KMeans(n_clusters=k, max_iter=500, n_init=100,
-                            random_state=0, init=np.reshape(seeds, (k, 1))).fit(data)
+            kmeans = KMeans(
+                n_clusters=k,
+                max_iter=500,
+                n_init=100,
+                random_state=0,
+                init=np.reshape(seeds, (k, 1)),
+            ).fit(data)
             data["clusters"] = kmeans.labels_
         else:
-            kmeans = KMeans(n_clusters=k, max_iter=300,
-                            n_init=100, random_state=0).fit(data)
+            kmeans = KMeans(n_clusters=k, max_iter=300, n_init=100, random_state=0).fit(
+                data
+            )
             data["clusters"] = kmeans.labels_
         # Inertia: Sum of distances of samples to their closest cluster center
         sse[k] = kmeans.inertia_
@@ -27,5 +33,6 @@ def elbow_plot(data, maxK=10, seed_centroids=None):
     plt.plot(list(sse.keys()), list(sse.values()))
     plt.show()
     return
+
 
 # elbow_plot(data)
